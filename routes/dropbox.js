ffff9a1c325828ad88ad66ee0e09b8e46f209c69
zina <127.0.0.1.zina@gmail.com>
2014-07-21 22:05:33 +0900

    delta cursorTag to cursor()

--- routes/dropbox.js
+++ routes/dropbox.js
@@ -186,7 +186,7 @@ var syncChangedMarkdown = function(stat, dbx) {
       },
       /* 5. update cursor */
       function(callback) {
-        crawler_repo.setCursor(stat.cursorTag, callback);
+        crawler_repo.setCursor(stat.cursor(), callback);
       }
     ],
     function(e) {
