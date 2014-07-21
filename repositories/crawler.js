3b64be6d5fc330c961ea8d70973cbb84fe5bf46f
zina <127.0.0.1.zina@gmail.com>
2014-07-22 00:28:12 +0900

    insert or update sql statement change to replace

--- repositories/crawler.js
+++ repositories/crawler.js
@@ -29,7 +29,7 @@ var crawler_repo = {
   },
   /* result_callback(err) */
   setCursor: function(cursor, result_callback) {
-    db.query('INSERT INTO crawler(`Cursor`) VALUES(?) ON DUPLICATE KEY UPDATE `Cursor` = ?', [cursor, cursor], function(e) {
+    db.query('REPLACE INTO crawler (`Cursor`) VALUES(?)', [cursor], function(e) {
       if (e) {
         result_callback(e);
         return;
